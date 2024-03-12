39a07f5893f5b57a92dba4cb016ce6f80a062d9a
User1 <Gigagogava67@gmail.com>
2024-03-12 17:35:48 +0400

    step1

---
+++ Selenium_Homework/src/main/java/Utils/UniversalSelector.java
@@ -0,0 +1,28 @@
+package Utils;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+
+public class UniversalSelector {
+    public static void universalSelector(Object element, String visibleText) {
+        if (element instanceof Select) {
+            Select select = (Select) element;
+            select.selectByVisibleText(visibleText);
+        } else if (element instanceof WebElement) {
+            WebElement webElement = (WebElement) element;
+            List<WebElement> options = webElement.findElements(By.tagName("option"));
+            for (WebElement option : options) {
+                if (option.getText().equals(visibleText)) {
+                    option.click();
+                    break;
+                }
+            }
+        } else {
+            throw new IllegalArgumentException("Invalid element type");
+        }
+        System.out.println("I AM A FIX");
+    }
+}
