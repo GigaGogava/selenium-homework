cd3209a18035b11192781c39176cefcb9a8f1fb7
User1 <Gigagogava67@gmail.com>
2024-03-12 16:31:51 +0400

    hotfix1_xD

--- Selenium_Homework/src/main/java/Utils/UniversalSelector.java
+++ Selenium_Homework/src/main/java/Utils/UniversalSelector.java
@@ -23,6 +23,6 @@ public class UniversalSelector {
         } else {
             throw new IllegalArgumentException("Invalid element type");
         }
-
+        System.out.println("I AM A FIX");
     }
 }
