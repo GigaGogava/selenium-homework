baf52ab6349d02bae3c6631042099788575d64ac
User1 <Gigagogava67@gmail.com>
2024-03-19 15:23:33 +0400

    added voidTest

--- Selenium_Homework/src/test/java/CookiesTest.java
+++ Selenium_Homework/src/test/java/CookiesTest.java
@@ -100,6 +100,6 @@ public class CookiesTest extends Constants {
         List<WebElement> suggestions = driver.findElements(By.cssSelector(DEMO_SUG_CSS));
         suggestions.get(0).click();
     }
-
+    public void voidTest(){}
 }
 
